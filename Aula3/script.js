97cbe7162e31b9bde23dd39187a00227025a8b23
cross <kalleoantunes@gmail.com>
2024-08-12 17:20:30 -0300

    conteúdo da aula 3

---
+++ Aula3/script.js
@@ -0,0 +1,31 @@
+/*alert("Boa noite pessual")
+const nome =prompt("qual teu nome?")
+const Snome =prompt("qual teu sobre-nome?")
+const idade=prompt("e a tua idade?")
+const aluno=prompt("você é estudante?")
+alert(`olá, meu nome é ${nome} ${Snome} e tenho ${idade} anos e ${aluno} sou estudante! `)
+
+const got= "Game of Thrones"
+const temporadasDeGot=8
+typeof got
+typeof temporadasDeGot
+
+console.log(typeof got)
+console.log(typeof temporadasDeGot)
+
+let novaVariavel
+console.log(typeof novaVariavel)
+
+novaVariavel=2
+
+console.log(typeof novaVariavel)
+
+novaVariavel=undefined
+
+console.log(typeof novaVariavel)*/
+
+
+const idadeNumero=23
+const idadeTexto="23"
+
+console.log(typeof idade)
