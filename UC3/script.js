11b96a6f60cb3aa572c68357938dab16a21a7650
Jessica Ferreira <jeporsh0@gmail.com>
2024-10-22 15:17:27 -0300

    adicionado planilha excel

---
+++ UC3/script.js
@@ -0,0 +1,38 @@
+/*
+SGBD
+Programa utilizado para manipular banco de dados 
+MySQL, Oracle, Paradox, Acess, Interbase 
+
+Vantagens
+▬Controle de redundância dos dados
+▬ Controle de acesso (segurança)
+▬ Armazenamento persistente dos dados;
+▬ Existência de multiplas interfacespara os usuários
+
+
+
+SBD
+Sistema de Banco de Dados 
+Sistema de manutenção de registro 
+
+
+Modelos de dados:
+▬▬ Modelos lógicos
+
+▬▬ Modelos Físicos:
+▬ É utilizado em SGBD para o modelo físico. Deve-se levar em conta as limitações impostas pelo SGBD escolhido e ser criado sempre com base
+nos exemplos.
+
+
+
+
+▬Tabelas: São os objetos
+▬Colunas:Armazenam caracteristas do banco de dados 
+▬Tipo dados: String, Number, etcc
+
+▬
+
+
+
+
+*/
