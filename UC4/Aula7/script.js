4f1f1450e0a9a22ea8e5d404a067ff01b0e6df62
Jessica Ferreira <jeporsh0@gmail.com>
2024-10-31 15:24:40 -0300

    adicionado Aula7

---
+++ UC4/Aula7/script.js
@@ -0,0 +1,57 @@
+/*
+Hierarquia e Herança 
+
+
+
+
+Polimorfismo
+
+
+
+
+
+
+Sobrecarca de método
+
+Assinatura de método:
+
+Somar(a:number,b:number)-> Number:{
+return a+b}
+
+
+Somar (a:integer,b:float)->float{}
+
+
+essas duas funções não possuem os mesmos parâmetros, então não
+podem estar dentro da mesma classe
+
+
+
+as assinaturas dos métodos precisam ser diferentes para que o código 
+funcione corretamente
+
+tmepo de compilação e tempo de execução
+
+
+
+
+
+SobreEscrita de método:
+você pode chamar o método e reescrever a função herdada dele 
+
+
+
+Vantagens Herança 
+▬ Modularidade
+▬ Extensibilidade
+▬ Manutenabilidade 
+
+Desvantagens
+▬ Acoplamento
+▬ Complexibilidade
+▬ Abuso
+
+
+
+*/
+
