e6eed439f0ad5d6eb2b53ea4fa0c6ed8717c5343
LukasLuut <lukasluut@gmail.com>
2025-02-03 17:11:53 -0300

    Simulado SQL

---
+++ UC4/Aula01_2025/index.ts
@@ -0,0 +1,14 @@
+/*
+método público pode ser acessado pelo usuário
+método privado só pode ser utilizado pela própria classe para realizar operações internas nos métodos públicos 
+
+
+HERANÇA-
+
+existe uma classe base que terá classes herdeiras que herdarão todos os atributos e métodos da classe base 
+exceto métodos privados, a não ser se utilizado PROTECT
+EX: 
+classe pessoa, que tem herdeiro como Pessoa física e outra de pessoa Jurídica 
+
+
+*/
