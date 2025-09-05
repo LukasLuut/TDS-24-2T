caf43bca0760f6335b42eb3a059356be397ee715
lukasluut <lukasluut@gmail.com>
2025-09-05 17:10:05 -0300

    aula do DTO

--- UC13/25-08/src/routes/auth.routes.ts
+++ UC13/25-08/src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 // src/routes/auth.routes.ts
 import { Router } from 'express'
 import { AuthController } from '../controllers/AuthController'
+import { CreateUserDTO } from '../dtos/LoginUserDTO'
+import { validateDTO } from '../middlewares/validateDTO'
 
 const router = Router()
 const controller = new AuthController()
 
-router.post('/register', controller.register.bind(controller))
+router.post('/register', validateDTO(CreateUserDTO), controller.register.bind(controller))
 router.post('/login', controller.login.bind(controller))
 
 export default router
